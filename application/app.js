7d333e9a60203d0025370336683441eae859dbc0
Tristan <thrawst88@gmail.com>
2014-07-11 19:58:39 -0600

    early data commit

--- application/app.js
+++ application/app.js
@@ -1,11 +1,21 @@
 var express = require('express');
+var database = require('./database').DataProvider;
 
 var app = express();
+
+// Configuration
+app.configure( function() {
+
+});
 
 app.use(express.static(__dirname + '/../public'));
 
 app.get('/', function (req, res) {
-  res.render('index.html');
+    res.render('index.html');
+});
+
+app.get('/list/:id', function (req, res) {
+    res.send("req id:" + req.params.id);
 });
 
 module.exports = app;
